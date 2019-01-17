f1f66fc45918af17dcf659aa5fc9cebd1bf793b9
Arunoday ray <arunoday.ray@full-cycle.com>
2019-01-17 16:19:31 +0530

    Changes with prime nova font and adding 1 week past incident

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'logger-ui';
+  arrayOfDays  = [];
+  ngOnInit() {
+    this.calDays();
+  }
+  calDays() {
+    for ( let i = 6; i >= 0 ; i--) {
+      this.arrayOfDays[i] = moment().subtract(i, 'days').format('MMMM Do, YYYY');
+    }
+  }
 }
